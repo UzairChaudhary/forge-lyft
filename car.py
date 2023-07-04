50745476c96c5043d585d0017de41761b79f7d17
Mohammad Uzair <100032005+UzairChaudhary@users.noreply.github.com>
2023-07-04 14:43:33 +0500

    commit

--- car.py
+++ car.py
@@ -1,9 +1,10 @@
-from abc import ABC, abstractmethod
+from serviceable import Serviceable
 
-class Car:
+class Car(Serviceable):
     def __init__(self, engine, battery):
         self.engine = engine
         self.battery = battery
-    @abstractmethod
+
     def needs_service(self):
         return self.engine.needs_service() or self.battery.needs_service()
+    
